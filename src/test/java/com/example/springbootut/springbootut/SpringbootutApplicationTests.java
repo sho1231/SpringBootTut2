99eaae12cd2f48419ee4cddba3e3d99ea3298101
Shourja Ganguly <shourja.ganguly181@gmail.com>
2024-06-20 22:24:50 +0530

    commit

--- src/test/java/com/example/springbootut/springbootut/SpringbootutApplicationTests.java
+++ src/test/java/com/example/springbootut/springbootut/SpringbootutApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootutApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
---
+++ src/test/java/com/example/springbootut/springbootut/service/DeptServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.springbootut.springbootut.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class DeptServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+    @Test
+    public void validDeptName(){
+        
+    }
+}
